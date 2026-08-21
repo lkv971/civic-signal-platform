285a2fee43c2d80d90b9e12650c51cabb7852e0f
Lee-roy <leeroyvaillant@outlook.com>
2026-08-21 16:00:46 -0400

    Add Silver transformation notebook scaffold

---
+++ fabric/NB_02_Silver_Transform_Core.Notebook/notebook-content.py
@@ -0,0 +1,40 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "9acb4c25-6c85-4c86-8fe0-2f648d8f670d",
+# META       "default_lakehouse_name": "LH_CivicSignal_Silver",
+# META       "default_lakehouse_workspace_id": "33c8bc67-ef39-4ee1-9049-d38e9d32e75e",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "9985f952-d718-475c-a462-a199fea986e3"
+# META         },
+# META         {
+# META           "id": "9acb4c25-6c85-4c86-8fe0-2f648d8f670d"
+# META         }
+# META       ]
+# META     },
+# META     "warehouse": {
+# META       "known_warehouses": []
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
